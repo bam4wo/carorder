197cb5391f52f1c465358fd633d43772901c2fa1
miia <miia@com.tw>
2019-11-05 13:11:13 +0800

    練習

---
+++ app/src/main/java/com/example/carorder/MainActivity.java
@@ -0,0 +1,72 @@
+package com.example.carorder;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText one;
+    private EditText two;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        one = findViewById(R.id.one);
+        two = findViewById(R.id.two);
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+    public void order(View view){
+        int onee = Integer.parseInt(one.getText().toString());
+        int twoo = Integer.parseInt(two.getText().toString());
+        int all = (onee*1000)+(twoo*2000)*8/10;
+        new AlertDialog.Builder(MainActivity.this)
+                .setTitle("訂購資訊")
+                .setMessage("單程票數量"+onee+"來回票數量"+twoo+"總價:"+all)
+                .setPositiveButton("完成",null)
+                .show();
+        Toast.makeText(MainActivity.this,"thank", Toast.LENGTH_LONG).show();
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
